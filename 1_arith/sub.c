83822ef25c2cd1be785b927ff6c9850ddbfed793
pragati29prerna <singh229pragati@gmail.com>
2021-11-30 22:13:55 -0800

    sub functionality

---
+++ 1_arith/sub.c
@@ -0,0 +1,12 @@
+/*including header files*/
+
+#include<stdio.h>
+#include"arith.h"
+
+    int sub(int a, int b)
+    {
+      int sub=0;
+      sub=a-b;
+      return sub; //returning sub value to the main
+    }
+
