1b1000f26864ec76983c88855d14ba138bb919bb
pragati29prerna <singh229pragati@gmail.com>
2021-11-30 22:46:08 -0800

    segmentation fault

---
+++ 4_segmentation/prog2/seg_fault2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    int *ptr=0x4000; //pointing to specific address
+    
+    ptr= (int *)malloc(sizeof(int) * 10); //allocating memory
+    printf("Address =%d\n",*ptr);
+    return 0;
+}
