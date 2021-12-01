acfc8dfc7840c481dca919b969bbc2ac0a274510
pragati29prerna <singh229pragati@gmail.com>
2021-11-30 22:04:58 -0800

    add and sub functionallity

---
+++ 1_arith/add.c
@@ -0,0 +1,11 @@
+/* including header files*/
+
+#include<stdio.h>
+#include"arith.h"
+
+    int add(int a, int b)
+    {
+      int sum=0;
+      sum=a+b;
+      return sum; //returning sum value to the main function
+    }
