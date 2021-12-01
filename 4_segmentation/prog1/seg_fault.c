b72d1d66bcac27349d3c57282c68e75d254f4b67
pragati29prerna <singh229pragati@gmail.com>
2021-11-30 22:38:54 -0800

    segmentation fault occured

---
+++ 4_segmentation/prog1/seg_fault.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int *ptr=NULL; //pointing to zero
+    printf("Address =%d\n",*ptr); //segmentation fault because zero is the special address
+    return 0;
+}
