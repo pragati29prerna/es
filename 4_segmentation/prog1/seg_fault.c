5bfe5b10ddeab8a38503a2ec0ea12af7a992915e
pragati29prerna <singh229pragati@gmail.com>
2021-12-02 02:23:15 -0800

    segmentation

--- 4_segmentation/prog1/seg_fault.c
+++ 4_segmentation/prog1/seg_fault.c
@@ -1,8 +1,10 @@
+
 #include<stdio.h>
 
 int main(int argc, char const *argv[])
 {
     int *ptr=NULL; //pointing to zero
     printf("Address =%d\n",*ptr); //segmentation fault because zero is the special address
+   
     return 0;
 }
