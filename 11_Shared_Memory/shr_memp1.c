8db9b54d7367ae788968a863531755dc2edaa772
pragati29prerna <singh229pragati@gmail.com>
2021-12-08 00:45:24 -0800

    shared memory p1 for user input

---
+++ 11_Shared_Memory/shr_memp1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/*including shared_memory header files*/
+#include <sys/mman.h>
+#include <sys/stat.h>        /* For mode constants */
+#include <fcntl.h>           /* For O_* constants */
+/*including ftruncate header files*/
+#include <unistd.h>
+#include <sys/types.h>
+#include<stdlib.h>
+
+int* count;
+int num;
+int main(int argc, char const *argv[])
+{
+int shmd;
+
+shmd= shm_open("/WELCOME",O_RDWR|O_CREAT,S_IRUSR|S_IWUSR);
+ftruncate(shmd,sizeof(int));
+count = (int*)mmap(NULL,sizeof(int),PROT_READ|PROT_WRITE,MAP_SHARED,shmd, 0);
+
+while(1)
+{
+    scanf("%d",&num);
+    *count=num;
+}
+return 0;
+}
