28e063b44cd4655df2169faf264bd5b275e1c35c
pragati29prerna <singh229pragati@gmail.com>
2021-12-09 06:29:17 -0800

    static library

---
+++ 2_static_lib/noida/proj.c
@@ -0,0 +1,18 @@
+/*including the standard header file and user defined header file*/
+
+#include<stdio.h>
+#include"arith.h"
+
+
+int main(int argc, char const *argv[])
+{
+    int result=0;//initialize the local variable
+
+    result=add(40,50,10); //calling add function
+    printf("Addition =%d\n",result);
+
+    result=sub(100,50);//calling sub function
+    printf("Substraction =%d\n",result);
+
+    return 0;
+}
