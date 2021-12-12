3f9df978858ddcbaa2046eca545f074ece78ff0a
pragati29prerna <singh229pragati@gmail.com>
2021-12-11 20:22:26 -0800

    send message

---
+++ 11_MsgQueue/mq_send.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/*msg queue header*/
+#include <fcntl.h>           /* For O_* constants */
+#include <sys/stat.h>        /* For mode constants */
+#include <mqueue.h>
+
+
+int main(int argc, char const *argv[])
+{
+mqd_t mqueue; //opaque datatype for msg_queue
+
+struct mq_attr myqueue_attr; //message queue attributes
+
+myqueue_attr.mq_flags = 0; /* Flags (ignored for mq_open()) */
+myqueue_attr.mq_maxmsg =4;  /* Max. # of messages on queue */
+myqueue_attr.mq_msgsize =128; /* Max. message size (bytes) */
+myqueue_attr.mq_curmsgs =0; /* # of messages currently in queue (ignored for mq_open()) */
+
+mqueue= mq_open("/mqueue",O_WRONLY|O_CREAT,S_IRUSR|S_IWUSR,&myqueue_attr); //Open the queue to send messages only.
+mq_send(mqueue,"HELLO",7,0);//send message to prog 1
+
+mq_close(mqueue);//close message queue
+   return 0;
+}
